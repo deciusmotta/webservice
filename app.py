03e9437798d99ad3fc8dd5574c8f890e619cb804
Décius <72174645+deciusmotta@users.noreply.github.com>
2025-10-10 13:01:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,46 +1,54 @@
-from flask import Flask, request
-from spyne import Application, rpc, ServiceBase, Unicode, Date, Iterable
+from flask import Flask, request, Response
+from spyne import Application, rpc, ServiceBase, Unicode, Integer
 from spyne.protocol.soap import Soap11
 from spyne.server.wsgi import WsgiApplication
-from spyne.interface.wsdl import Wsdl11
-from io import BytesIO
+import logging
 
-class Iwbsfunctions_017(ServiceBase):
-    @rpc(Unicode, Unicode, Unicode, Date, Unicode, _returns=Iterable(Unicode))
-    def EmitirLaudo(ctx, CNPJEmpresa, NumeroLaudo, NomeCliente, DataExecucao, Observacoes):
-        prefixo = "017"
-        numero_completo = f"{prefixo}-{NumeroLaudo}"
-        mensagem = f"Laudo emitido com sucesso para {NomeCliente} em {DataExecucao}."
-        codigo_retorno = "0"
-        yield f"CodigoRetorno={codigo_retorno}"
-        yield f"MensagemRetorno={mensagem}"
-        yield f"NumeroLaudoCompleto={numero_completo}"
+logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
 
+# --- Serviço SOAP ---
+class LaudoService(ServiceBase):
+    @rpc(Integer, _returns=Unicode)
+    def gerar_laudo(ctx, numero):
+        """
+        Gera um laudo cujo identificador completo começa com 017
+        """
+        return f"017-{numero:06d}"
+
+# --- Configuração do Spyne ---
 soap_app = Application(
-    [Iwbsfunctions_017],
-    tns='http://laudoservice.onrender.com/wsdl/Iwbsfunctions_017',
+    [LaudoService],
+    tns='http://laudoservice.onrender.com/soap',
+    name='LaudoService',
     in_protocol=Soap11(validator='lxml'),
     out_protocol=Soap11()
 )
 
 wsgi_app = WsgiApplication(soap_app)
 
-@app.route('/soap', methods=['GET', 'POST'])
-def soap_service():
-    if request.method == 'GET':
-        wsdl = Wsdl11(soap_app)
-        wsdl_document = wsdl.build_interface_document('https://laudoservice.onrender.com/soap')
-        return app.response_class(wsdl_document, content_type='text/xml; charset=utf-8')
-    elif request.method == 'POST':
-        response = BytesIO()
-        wsgi_app(request.environ, response.write)
-        return app.response_class(response.getvalue(), content_type='text/xml; charset=utf-8')
 
-@app.route('/')
+@app.route("/soap", methods=["POST"])
+def soap_server():
+    """Rota principal para requisições SOAP"""
+    response = Response()
+    response.headers["Content-Type"] = "text/xml; charset=utf-8"
+    response.data = wsgi_app(request.environ, response.start_response)
+    return response
+
+
+@app.route("/soap?wsdl", methods=["GET"])
+def wsdl():
+    """Gera e retorna o WSDL dinamicamente"""
+    wsdl_content = soap_app.get_interface_document('wsdl')
+    return Response(wsdl_content, mimetype='text/xml')
+
+
+@app.route("/")
 def home():
-    return "<h2>🧾 LaudoService SOAP ativo</h2><p>Endpoint: /soap</p><p>WSDL: /soap?wsdl</p>"
+    return "LaudoService SOAP ativo em /soap e WSDL em /soap?wsdl"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
